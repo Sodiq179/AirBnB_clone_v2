b36694521f843da8954bc9a5e220ebb8e42f39b3
Sodiq179 <babawalesodiq996@gmail.com>
2022-09-14 23:15:36 +0100

    [Update]: Database columns relationship

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines the User class."""
+from os import getenv
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
@@ -20,8 +21,15 @@ class User(BaseModel, Base):
         reviews (sqlalchemy relationship): The User-Review relationship.
     """
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
+    if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", backref="user", cascade="delete")
+        reviews = relationship("Review", backref="user", cascade="all, delete, delete-orphan")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
