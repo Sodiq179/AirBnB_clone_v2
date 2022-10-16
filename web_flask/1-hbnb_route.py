f036c0860cfdb28d0562b71f72f2e0c964f27509
Sodiq179 <babawalesodiq996@gmail.com>
2022-10-16 19:30:16 +0000

    Add debug argument

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,7 +3,7 @@
 script starts Flask web app
     listen on 0.0.0.0, port 5000
     routes: /: display "Hello HBNB!"
-    routes: /hbnb: display “HBNB”
+    routes: /hbnb: display HBNB
 """
 
 from flask import Flask
@@ -12,15 +12,15 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-	"""This returns Hello HBNB message at the root"""
+	"""Returns Hello HBNB!"""
 	return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb_page():
-	"""This page displays HBNB"""
+	"""Displays HBNB"""
 	return "HBNB"
 
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000)
+	app.run(host="0.0.0.0", port=5000, debug = True)
