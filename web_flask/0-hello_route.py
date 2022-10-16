eca3f6689a39218bf4035ce6aa50f24005662118
Sodiq179 <babawalesodiq996@gmail.com>
2022-10-16 18:53:48 +0000

    Debugging

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -14,5 +14,6 @@ def hello_hbnb():
 	"""This returns Hello HBNB message at the root"""
 	return "Hello HBNB!"
 
+
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port:5000)
+	app.run(host="0.0.0.0", port=5000)
