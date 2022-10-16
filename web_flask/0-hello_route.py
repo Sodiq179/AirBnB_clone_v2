60d429c0077d0a95d62c51f69081310c7a9cc46f
Sodiq179 <babawalesodiq996@gmail.com>
2022-10-16 18:43:44 +0000

    Adding Documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+"""This script runs an app with Flask framework"""
 from flask import Flask
+
 
 # Creating an app instance
 app = Flask(__name__)
 
+
 # Creating the root of the web application
-@app.route("/")#, strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def root():
-	# This returns Hello HBNB message at the root 
-	# of the web app
+	"""This returns Hello HBNB message at the root"""
 	return "Hello HBNB!"
 
+if __name__ == "__main__":
+	app.run(host='0.0.0.0'. port:5000)
