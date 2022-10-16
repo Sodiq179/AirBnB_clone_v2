eea5c5bfe645816fa04ba493efaf78b7ac5d2adb
Sodiq179 <babawalesodiq996@gmail.com>
2022-10-16 18:49:10 +0000

    Debugging

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""This script runs an app with Flask framework"""
+"""
+script starts Flask web app
+    listen on 0.0.0.0, port 5000
+    routes: /: display "Hello HBNB!"
+"""
+
 from flask import Flask
-
-
-# Creating an app instance
 app = Flask(__name__)
 
 
-# Creating the root of the web application
 @app.route("/", strict_slashes=False)
-def root():
+def hello_hbnb():
 	"""This returns Hello HBNB message at the root"""
 	return "Hello HBNB!"
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0'. port:5000)
+	app.run(host="0.0.0.0", port:5000)
