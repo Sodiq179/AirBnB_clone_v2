aa8a7f8deaaa2d72606475619ee1aa1a1dd7e6cf
Sodiq179 <babawalesodiq996@gmail.com>
2022-10-19 18:32:21 +0000

    Initial commit

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+script starts Flask web app
+    listen on 0.0.0.0, port 5000
+    routes: /: display "Hello HBNB!"
+    routes /hbnb: displays "HBNB"
+    routes /c/<text>: diaplays C text
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+	"""return Hello HBNB"""
+	return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_page():
+	"""return HBNB"""
+	return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+	"""C is fun"""
+	return "C {}".format(text)
+
+
+if __name__ == "__main__":
+	app.run(host = "0.0.0.0", port=5000, debug = True)
