4719e8483c0352d41298ea3742faf7b2c98eeb47
Sodiq179 <babawalesodiq996@gmail.com>
2022-10-19 18:43:21 +0000

    Initial commit

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+script starts Flask web app
+    listen on 0.0.0.0, port 5000
+    routes: /: display "Hello HBNB!"
+    routes /hbnb: displays "HBNB"
+    routes /c/<text>: diaplays C text
+    routes /python/(<text>): displays Python text
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+	"""return Hello HBNB"""
+	return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_page():
+	"""return HBNB"""
+	return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+	"""C is fun"""
+	text = " ".join(text.split("_"))
+	return "C {}".format(text)
+
+@app.route("/python/(<text>)", strict_slashes=False)
+def python_is_fun(text = "is cool"):
+	"Python is cool"
+	text = " ".join(text.split("_"))
+	return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+	app.run(host = "0.0.0.0", port=5000, debug = True)
