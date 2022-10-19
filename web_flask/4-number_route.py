19754d8728d6a17d89d60db62cd32c16205ed265
Sodiq179 <babawalesodiq996@gmail.com>
2022-10-19 19:40:04 +0000

    Initial commit

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+script starts Flask web app
+    listen on 0.0.0.0, port 5000
+    routes: /: display "Hello HBNB!"
+    routes /hbnb: displays "HBNB"
+    routes /c/<text>: diaplays C text
+    routes /python/(<text>): displays Python text
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+	"""return Hello HBNB"""
+	return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_page():
+	"""return HBNB"""
+	return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+	"""C is fun"""
+	text = " ".join(text.split("_"))
+	return "C {}".format(text)
+
+@app.route('/python/', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_fun(text = "is cool"):
+	"Python is cool"
+	if text != "is cool":
+		text = " ".join(text.split("_"))
+	return "Python {}".format(text)
+
+@app.route('/number/<n>', strict_slashes=False)
+def is_number(n):
+	"""Check number"""
+	if type(n) == int:
+		return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+	app.run(host = "0.0.0.0", port=5000, debug = True)
