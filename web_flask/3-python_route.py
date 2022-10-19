45f0afc126498282ee0587d2802db23ebf79577d
Sodiq179 <babawalesodiq996@gmail.com>
2022-10-19 18:45:35 +0000

    Initial commit

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,7 +27,7 @@ def c_is_fun(text):
 	text = " ".join(text.split("_"))
 	return "C {}".format(text)
 
-@app.route("/python/(<text>)", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def python_is_fun(text = "is cool"):
 	"Python is cool"
 	text = " ".join(text.split("_"))
